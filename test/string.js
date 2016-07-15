0eea60241daaaa8b517870f2dbdc211da935f440
Tyler Stewart <tylers490@gmail.com>
2016-07-15 13:24:27 -0600

    Add empty string to string conversion

--- test/string.js
+++ test/string.js
@@ -8,6 +8,8 @@ describe('string', function () {
   it('converts undefined to "undefined"', () => expect(as.string(undefined, {allowUndefined: false})).to.equal('undefined'));
   it('converts "null" to "null"', () => expect(as.string('null')).to.equal('null'));
   it('converts "undefined" to "undefined"', () => expect(as.string('undefined')).to.equal('undefined'));
+  it('converts "" to null', () => expect(as.string('')).to.equal(null));
+  it('converts "" to ""', () => expect(as.string('', {allowNull: false})).to.equal(''));
   it('converts 1 to "1"', () => expect(as.string(1)).to.equal('1'));
   it('converts 1.1 to "1.1"', () => expect(as.string(1.1)).to.equal('1.1'));
   it('converts false to "false"', () => expect(as.string(false)).to.equal('false'));
